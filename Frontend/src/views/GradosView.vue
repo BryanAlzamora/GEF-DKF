972caa56b2e701b27f043f01cd06579fc9e4b9f5
BryanAlzamora <161702024+BryanAlzamora@users.noreply.github.com>
2026-01-25 00:03:43 +0100

    d

--- Frontend/src/views/GradosView.vue
+++ Frontend/src/views/GradosView.vue
@@ -9,12 +9,12 @@
 
     function mostrarAsignaturas(grado) {
         gradoSeleccionado.value = grado;
-        vistaActual.value = 'asignaturas';
+        vistaActual.value = grado ? 'asignaturas' : null;
     }
 
     function mostrarCompetencias(grado) {
         gradoSeleccionado.value = grado;
-        vistaActual.value = 'competencias';
+        vistaActual.value = grado ? 'competencias' : null;
     }
 </script>
 
@@ -29,9 +29,19 @@
             />
 
             <GradoDatos 
+                v-if="gradoSeleccionado"
                 :grado="gradoSeleccionado" 
                 :vista="vistaActual" 
             />
+            
+            <div v-else class="col-12 col-lg-8 mt-3">
+                <div class="card shadow-sm">
+                    <div class="card-body text-center text-muted py-5">
+                        <i class="bi bi-building" style="font-size: 4rem;"></i>
+                        <p class="mt-3">Selecciona un grado para ver sus asignaturas o competencias</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
